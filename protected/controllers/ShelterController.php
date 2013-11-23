9e9f5abf07024eb8016c470c23633e3f0b03c1d0
dmeikle <davemeikle@ymail.com>
2013-11-23 12:54:00 -0800

    added GUI for editing/creating shelter

--- protected/controllers/ShelterController.php
+++ protected/controllers/ShelterController.php
@@ -3,30 +3,39 @@
 class ShelterController extends Controller
 {
 
-    public function actionIndex() {
+    public function actionIndex()
+    {
         //fetch all shelters
         $shelters = Shelters::model()->findAll();
 
-        $this->render("/shelter/index/main", array(
-            'shelters' => $shelters
-        ));
+        $this->render("/shelter/index/main", array('shelters' => $shelters));
     }
 
-    public function actionEdit() {
+    public function actionEdit()
+    {
         $shelterId = Yii::app()->input->get("id");
 
         $shelter = Shelters::model()->findByPk($shelterId);
+        $cities = Cities::model()->findAll();
 
-            $this->render("/shelter/edit/main", array(
-                'shelter' => $shelter
-            ));
+ //TODO       //this will be the logged in user's ID
+        $userId = 18;
+
+        $this->render("/shelter/edit/main", array(
+            'shelter' => $shelter,
+            'cities' => $cities,
+            'userId' => $userId
+        ));
     }
 
-    public function actionRemove() {
+    public function actionRemove()
+    {
 
     }
 
-    public function actionSave() {
+    public function actionSave()
+    {
 
     }
+
 }
