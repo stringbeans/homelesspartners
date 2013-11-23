074346d2546601e1114021572a907810e0904ae5
John Kor <jkor@eslexplorer.com>
2013-11-23 10:20:42 -0800

    adding model for shelter dropoffs

---
+++ protected/models/ShelterDropoffs.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This is the model class for table "shelter_dropoffs".
+ *
+ * The followings are the available columns in table 'shelter_dropoffs':
+ * @property string $dropoff_id
+ * @property string $shelter_id
+ * @property string $name
+ * @property string $address
+ * @property string $notes
+ */
+class ShelterDropoffs extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'shelter_dropoffs';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('shelter_id', 'required'),
+			array('shelter_id', 'length', 'max'=>11),
+			array('name, address', 'length', 'max'=>255),
+			array('notes', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('dropoff_id, shelter_id, name, address, notes', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'dropoff_id' => 'Dropoff',
+			'shelter_id' => 'Shelter',
+			'name' => 'Name',
+			'address' => 'Address',
+			'notes' => 'Notes',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('dropoff_id',$this->dropoff_id,true);
+		$criteria->compare('shelter_id',$this->shelter_id,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('address',$this->address,true);
+		$criteria->compare('notes',$this->notes,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return ShelterDropoffs the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
