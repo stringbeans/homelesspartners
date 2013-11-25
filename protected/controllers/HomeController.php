b859ed433aa904525bb743ad28798c8bd21c13bc
aastfalk <aastfalk@gmail.com>
2013-11-24 18:29:56 -0800

    When pledging, confirmation email is sent

--- protected/controllers/HomeController.php
+++ protected/controllers/HomeController.php
@@ -13,4 +13,31 @@ class HomeController extends Controller
         //$this->render('/home/static/shelterstories');
         $this->render('/home/static/story');
 	}
+
+	public function actionContact()
+	{
+		$this->render('/home/contact/main');
+	}
+
+	public function actionContactProcessor()
+	{
+		$name = Yii::app()->input->post('name');
+		$email = Yii::app()->input->post('email');
+		$body = Yii::app()->input->post('body');
+
+		$body = 'Name: ' . $name . "\n" . 'Email: ' . $email . "\n\n" . $body;
+
+		$email = new Email();
+		$result = $email->send(Yii::app()->params['HP_SENDER_EMAIL_ADDRESS'], Yii::app()->params['HP_SENDER_EMAIL_ADDRESS'], 'Contact Us Request', $body);
+	
+		if($result)
+		{
+			Yii::app()->user->setFlash('success', 'Thanks for contacting us.');
+		}
+		else
+		{
+			Yii::app()->user->setFlash('error', '');
+		}
+		$this->redirect($this->createUrl('home/contact'));
+	}
 }
