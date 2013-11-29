04a17d4d847b4020f33fdfe466381dfadf52ce24
John Kor <jkor@eslexplorer.com>
2013-11-28 22:06:25 -0800

    added favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,9 +2,12 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
+		
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="language" content="en" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="icon" type="image/ico" href="/images/favicon.ico">
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
