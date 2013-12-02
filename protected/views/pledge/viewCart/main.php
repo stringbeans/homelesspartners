deaa703c6d258a92ed5315b4b16effc11373cf76
Mike Martn <mikemartin604@gmail.com>
2013-12-01 23:03:01 -0800

    Updated Pledge cart styles and restored Selectize.bootstrap3

--- protected/views/pledge/viewCart/main.php
+++ protected/views/pledge/viewCart/main.php
@@ -69,128 +69,107 @@
 	});
 </script>
 
-<div class='container'>
-	<div class='row'>
-		<div class='col-md-12'>
-
-			<h2>Pledge Cart</h2>
-
-			<?php if(Yii::app()->user->hasFlash('success')): ?>
-		 	<div class="alert alert-success">
-		    <?php echo Yii::app()->user->getFlash('success'); ?>
-			</div>
-		 	<?php endif; ?>
-
-		 	<?php if(Yii::app()->user->hasFlash('error')): ?>
-		 	<div class="alert alert-danger">
-		    <?php echo Yii::app()->user->getFlash('error'); ?>
-			</div>
-		 	<?php endif; ?>
-
-			
-			<p>
-				You currently have <strong><?php echo $totalGifts ?> gifts</strong> in your pledge cart for <strong><?php echo sizeof($pledgeCartInfo) ?> shelter lists</strong>. 
-				Please confirm each gift, provide an estimated delivery date, and complete your pledge(s). 
-				Please review the notes section for each drop off location.
-			</p>
-		</div>
-	</div>
-</div>
-
-<?php if(!empty($pledgeCartInfo)): ?>
-<form id='pledgeCheckoutForm' action='<?php echo $this->createUrl("pledge/confirmPledges") ?>' method='post'>
-
-<?php foreach ($pledgeCartInfo as $shelterId => $shelter): ?>
-	<div class='container' style='background-color: #fffeee; margin-top: 20px; border: 2px solid black'>
+<section>
+	<div class='container'>
 		<div class='row'>
-			<div class='col-md-12'>
-				<table class='table'>
-					<tr>
-						<td style='width: 1px;'><img src='http://placehold.it/150x150' /></td>
-						<td>
-							<h4><?php echo $shelter['name']; ?></h4>
-							<strong><?php echo $shelter['city'] ?>, <?php echo $shelter['region'] ?></strong> <br/>
-							<?php if(!empty($shelter['website'])): ?>
-							<a href='<?php echo $shelter['website'] ?>'><?php echo $shelter['website'] ?></a>
-							<?php endif; ?>
-						</td>
-						<td class='text-center'>
-							<h6>Gifts for this shelter</h6>
-							<h4 id='giftCount'><?php echo $shelter['giftCount'] ?></h4>
-						</td>
-					</tr>
-				</table>
-			</div>
-		</div>
-		<?php if(!empty($shelter['dropoffLocations'])): ?>
-		<div class='row'>
-			<div class='col-md-12'>
-				<table class='table'>
-					<?php foreach ($shelter['dropoffLocations'] as $location): ?>
-					<tr>
-						<td rowspan='3' style='width: 1px'>Drop off location</td>
-						<td><label><strong>Name:</strong></label> <?php echo $location->name ?></td>
-					</tr>
-					<tr>
-						<td><label><strong>Address:</strong></label> <?php echo $location->address ?></td>
-					</tr>
-					<tr>
-						<td><label><strong>Notes:</strong></label> <?php echo $location->notes ?></td>
-					</tr>	
-					<?php endforeach ?>
-				</table>
-			</div>
-		</div>
-		<?php endif; ?>
-		<div class='row'>
-			<div class='col-md-12'>
-				<table class='table'>
-					<tbody>
-						<?php foreach($shelter['stories'] as $story): ?>
-						<tr>
-							<th>Name: <?php echo $story['fname'] ?> <?php echo $story['lname'] ?></th>
-							<th class='text-right'>ID: <?php echo $story['assignedId'] ?></th>
-						</tr>
-							<?php foreach ($story['gifts'] as $gift): ?>
+			<div class='col-md-10 col-md-offset-1'>
+        <div class="page-header">
+          <h3>Pledge Cart</h3>
+        </div>
+        <?php if(Yii::app()->user->hasFlash('success')): ?>
+	       <div class="alert alert-success">
+            <button type="button" class="close fui-cross" data-dismiss="alert"></button>
+            <?php echo Yii::app()->user->getFlash('success'); ?>
+         </div>
+        <?php endif; ?>
+        <?php if(Yii::app()->user->hasFlash('error')): ?>
+        	<div class="alert alert-error">
+            <button type="button" class="close fui-cross" data-dismiss="alert"></button>
+            <?php echo Yii::app()->user->getFlash('error'); ?>
+          </div>
+        <?php endif; ?>
+				<p>
+					You currently have <strong><?php echo $totalGifts ?> gifts</strong> in your pledge cart for <strong><?php echo sizeof($pledgeCartInfo) ?> shelter lists</strong>. 
+					Please confirm each gift, provide an estimated delivery date, and complete your pledge(s). 
+					Please review the notes section for each drop off location.
+				</p>
+
+				<?php if(!empty($pledgeCartInfo)): ?>
+				<form id='pledgeCheckoutForm' class="form-horizontal" action='<?php echo $this->createUrl("pledge/confirmPledges") ?>' method='post'>
+					<?php foreach ($pledgeCartInfo as $shelterId => $shelter): ?>
+					<div class="panel panel-default pledge-checkout">
+					  <!-- Default panel contents -->
+					  <div class="panel-heading">
+					    <div class="media">
+							  <a class="pull-left hidden-xs" href="#">
+							    <img class="media-object" src="http://placehold.it/80x80" alt="...">
+							  </a>
+							  <div class="media-body pull-left">
+							    <h4 class="media-heading"><?php echo $shelter['name']; ?><br/><small><?php echo $shelter['city'] ?>, <?php echo $shelter['region'] ?></small></h4>
+							  </div>
+							  <div class="stat-metric pull-right hidden-xs">
+											<h4 class="stat-name">Total Gifts</h4>
+											<strong class="stat-count"><?php echo $shelter['giftCount'] ?></strong>
+							  </div>
+							</div>
+					  </div><!-- /panel-heading -->
+					  <div class="panel-body">
+					  	<div class="row">
+				    		<div class="col-sm-7">
+				    			<div class="form-group">
+				    				<label class="col-sm-6 control-label">Estimated delivery date</label>
+				          	<div class="col-sm-6 input-group">
+				          	  <span class="input-group-btn">
+				          	  	<button class="btn" type="button"><span class="fui-calendar"></span></button>
+				          	  </span>
+				          	  <input type="text" class="form-control datepicker" name='deliveryDate[<?php echo $shelterId ?>]' value="">
+				          	</div>
+				          </div>
+				    		</div>
+				    	</div>
+							<table class='table'>
+								<?php foreach ($shelter['dropoffLocations'] as $location): ?>
 								<tr>
-									<td><?php echo $gift['description'] ?></td>
-									<td class='text-right'>
-										<a href='#' class='btn btn-danger remove' data-id='<?php echo $gift['giftId'] ?>'><span class='glyphicon glyphicon-gift'></span> Unpledge Gift</a>
-										<input type='hidden' name='giftId[]' value='<?php echo $gift['giftId'] ?>' />
-									</td>
+									<td rowspan='3' style='width: 1px'>Drop off location</td>
+									<td><label><strong>Name:</strong></label> <?php echo $location->name ?></td>
 								</tr>
-							<?php endforeach ?>
-						
-						<?php endforeach; ?>
-						<tr>
-							<td>Estimated delivery date:</td>
-							<td style='width: 300px;'>
+								<tr>
+									<td><label><strong>Address:</strong></label> <?php echo $location->address ?></td>
+								</tr>
+								<tr>
+									<td><label><strong>Notes:</strong></label> <?php echo $location->notes ?></td>
+								</tr>	
+								<?php endforeach ?>
+							</table>
+					  </div>
 
-								<div class="form-group">
-						          	<div class="input-group">
-						          	  <span class="input-group-btn">
-						          	  	<button class="btn" type="button"><span class="fui-calendar"></span></button>
-						          	  </span>
-						          	  <input type="text" class="form-control datepicker" name='deliveryDate[<?php echo $shelterId ?>]' value="">
-						          	</div>
-						          </div>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+						<ul class="list-group">
+							<?php foreach($shelter['stories'] as $story): ?>
+					    <li class="list-group-item">
+					    	<h4 class="list-group-item-heading"><?php echo $story['fname'] ?> <?php echo $story['lname'] ?> <small><?php echo $story['assignedId'] ?></small></h4>
+						    <table class="table">
+			            <tbody>
+			            	<?php foreach ($story['gifts'] as $gift): ?>
+			              <tr class="checkout-item">
+			                <td><?php echo $gift['description'] ?></td>
+			                <td><a href='#' class='btn btn-danger remove' data-id='<?php echo $gift['giftId'] ?>'><span class='glyphicon glyphicon-gift'></span> Unpledge Gift</a>
+													<input type='hidden' name='giftId[]' value='<?php echo $gift['giftId'] ?>' /></td>
+			              </tr>
+			              <?php endforeach ?>
+			            </tbody>
+			          </table>
+					    </li>
+					    <?php endforeach; ?>
+
+					  </ul><!-- /list-group -->
+					</div><!-- /pledge-checkout -->
+					<?php endforeach ?>
+					<button type="submit" class="btn btn-hg btn-success pull-right">
+				    <span class="fui-check-inverted"></span> Complete Pledge Checkout
+				  </button>
+				</form>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
-<?php endforeach ?>
-
-<div class='container' style='margin-top: 20px;'>
-	<div class='row'>
-		<div class='col-md-12 text-center'>
-
-			<input type='submit' class='btn btn-success col-md-8 col-md-offset-2' value='Complete Pledge Checkout' />
-		</div>
-	</div>
-</div>
-
-</form>
-<?php endif; ?>
+</section>
