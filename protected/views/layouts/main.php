23032ff059896038bfaac3723581fe221fd2fe96
John Kor <jkor@eslexplorer.com>
2013-12-03 18:03:58 -0800

    added GA code

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -28,6 +28,17 @@
 	</head>
 
 	<body class="<?php echo $this->getBodyClasses()?>">
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44072359-1', 'homelesspartners.com');
+  ga('send', 'pageview');
+
+</script>
+
 		<?php $this->renderPartial("/layouts/_header") ?>
 
 		<?php echo $content; ?>
