477322ed81036371b711cc6ebf337601af70760f
Jamie Chong <jamie@jamiechong.ca>
2013-12-06 11:05:25 -0800

    bootstrap css must be on same server for respond.js to work in IE8

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -10,7 +10,11 @@
 		<link rel="icon" type="image/ico" href="/images/favicon.ico">
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+		<?php 
+		//bootstrap must come from same server for respond.js to work on IE8
+		//<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+		?>
+		<link href="<?php echo Yii::app()->baseUrl ?>/css/bootstrap.min.css" rel="stylesheet">
 		<link href="<?php echo Yii::app()->baseUrl ?>/css/flat-ui.css" rel="stylesheet">
 		<link href="<?php echo Yii::app()->baseUrl ?>/css/animate.min.css" rel="stylesheet">
 		<link href="<?php echo Yii::app()->baseUrl ?>/css/style.css" rel="stylesheet">
@@ -20,8 +24,8 @@
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	    <!--[if lt IE 9]>
-	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	      <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 	    <![endif]-->
 
 		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
