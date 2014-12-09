c8bf7c988b77d8fad30d590bfcb72c93280c3b1c
Kenny <kennywgrant@gmail.com>
2014-12-09 10:47:15 -0800

    Pushed new spread the word page

---
+++ protected/views/home/spread-the-word/main.php
@@ -0,0 +1,146 @@
+<section>
+	<div class='container'>
+		<div class='row'>
+			<div class="col-md-9 center-block">
+				<h2>Spread The Word</h2>
+				<p class="lead">Get involved, help spread our message.</p>
+				<p>Every aspect of our program is powered by people like you and word of mouth marketing. We've assembled a few methods below to help spread the #EndHomelessness message to more people around the world.  
+					</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="read-stories">
+	<div class="city-stories container">
+		<h2 class="section-title">Share Homeless Partners Posters</h2>
+		<div class="row">
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=18" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" class="img-responsive"/>
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=4" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" class="img-responsive"/>
+					
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=7" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" class="img-responsive"/>
+					
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=29" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" border ="1" class="img-responsive"/>
+					
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=32" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" class="img-responsive"/>
+					
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="city/cityShelters?id=33" class="city-tile">
+					<img src="https://s3.amazonaws.com/f.cl.ly/items/3Y0e1V1E0u0w353N1t25/default-poster-image.jpg" class="img-responsive"/>
+					
+				</a>
+				<center>
+   					<h4>Poster 1 Title</h4>
+   					<p><a href="">Save Image</a> | <a href="">PDF</a> | <a href="">Print</a></p>
+                  <p></p>
+   				</center>
+			</div>
+			
+			
+			
+		</div>
+	</div>
+</section>
+
+<section class="section-offset">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9 center-block">
+        
+        <h3 class="section-title">Email Friends and Family</h3>
+        
+        <p class="lead">We've created a basic email script to encourage close friends and family to get involved. Simply copy and paste the email below:</p>
+
+
+
+
+<p><b>To:</b> (<i><u>all of your friends and family</u></i>)
+<p><b>Subject Line:</b>	Local homeless people need our help</p>
+<p><b>Body:</b></p>
+
+<p>Dear Friends/,<i><u>FRIENDS NAME</u></i></p>
+
+<p>I’m sharing a cause that really means a lot to me. I’m participating in a program called the Christmas Wish List organized by Homeless Partners, a non-profit, volunteer-run organization that’s currently run in 9 different Canadian cities this year, including our hometown, <i><u>(NAME OF YOUR CITY)</i></u>.</p> 
+
+<p>The homeless are more than just a statistic; they each have their own stories. To recognize that, volunteers for this program go out and speak to homeless recipients, record their stories and wish lists, which are then uploaded to www.homelesspartners.com for the public to read and make a pledge. </p>
+
+<p>The pledges are personal items that the homeless recipients have requested for, but as you know small gifts can make a big impact. Homeless Partners can help bridge the gap between people like you and I with those who are less fortunate, so we can make a difference and help them step out of the cycle of homelessness. </p>
+
+<p>My goal is to make (<i><u>#INSERT_NUMBER</u></i>) of pledged gifts this year, and I need your involvement! Send me an email stating your interest and we can do this together to make a difference in the lives of the less fortunate!</p>
+
+<p><b>HOW TO MAKE A PLEDGE: </b>
+<il>
+	<ul>Visit <a href="http://homelesspartners.com">www.homelesspartners.com</a></ul>
+	<ul>Read and choose your favourite stories and wish lists from your local community</ul>
+	<ul>Make a pledge online and print out your record</ul>
+	<ul>Drop off a wrapped gift at a corresponding shelter with a caring note</ul>
+</il>
+
+<p>Will you join me? Every bit counts.</p>
+<p>Thanks,</p>
+<p><i><u>(YOUR NAME)</i></u></p>
+
+
+
+ 
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+	<div class="action container">
+		<p class="lead">Quick Email Links</p>
+		<a href="#" class="btn btn-inverse btn-hg">Email Client</a>
+		<a href="#" class="btn btn-inverse btn-hg">Gmail</a>
+		<a href="#" class="btn btn-inverse btn-hg">Yahoo Mail</a>
+		<a href="#" class="btn btn-inverse btn-hg">Hotmail / Live Mail</a>
+		</div>
+</section>
+
