c035552246bfa0e7b01521d85bb3b5f1e8f8a193
Vadim Vorotilov <fant.geass@gmail.com>
2014-12-11 02:05:34 +0600

    pledge day 2014 & spread the word pages

---
+++ protected/views/home/pledge/main.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Pledge day 2014</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Pledge Day 2014. A day to give back to the community.">
+    <meta name="author" content="Homeless Partners">
+    <meta name="keywords" content="Homeless Partners, Pledge Day 2014, Vancouver Charity, Homeless Charity Vancouver">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Loading Bootstrap -->
+    <link href="pledge_day_2014/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Loading Flat UI Pro -->
+    <link href="pledge_day_2014/css/flat-ui-pro.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="pledge_day_2014/js/vendor/html5shiv.js"></script>
+    <script src="pledge_day_2014/js/vendor/respond.min.js"></script>
+    <![endif]-->
+    <link href="pledge_day_2014/css/landingpagestyle.css" rel="stylesheet">
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-57436905-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <script src="pledge_day_2014/js/jquery.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+
+<body>
+<div id="fb-root"></div>
+<!-- Page Content -->
+<div class="container">
+    <!-- Page Header -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Pledge Day 2014</h1>
+            <p>A day to engage with the homeless people in your community and make an impact.
+                Give a small gift and make a big difference.</p>
+            <div id="snowflake"><img src="pledge_day_2014/images/PNG/snowflakedivider.png" alt="snowflake"></div>
+            <h2>5 Ways to Get Involved</h2>
+            <br>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Projects Row -->
+    <div class="row">
+        <div id="seperaterow">
+            <div class="col-md-2"> </div>
+            <div class="col-md-4">
+                <div class="transparentbox">
+                    <img class="img-responsive" src="pledge_day_2014/images/SVG/gift-box.svg" alt="Give Gifts">
+                    <h3>
+                        Read Stories <br>&amp; Pledge Gifts
+                    </h3>
+                    <div class="btnbox"><a href="http://www.homelesspartners.com/" class="btn btn-block btn-primary">View Stories</a></div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="transparentbox">
+                    <img class="img-responsive" src="pledge_day_2014/images/SVG/box.svg" alt="Donate Supplies">
+                    <h3>
+                        Drop Off Supplies <br>at a Local Shelter
+                    </h3>
+                    <div class="btnbox"><a href="http://www.homelesspartners.com/" class="btn btn-sm btn-block btn-primary">List of Shelters</a></div>
+                </div>
+            </div>
+            <div class="col-md-2"> </div>
+        </div>
+    </div>
+
+    <!-- /.row -->
+
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-2"> </div>
+        <div class="col-md-4 portfolio-item">
+            <div class="transparentbox">
+                <img class="img-responsive" src="pledge_day_2014/images/SVG/medal.svg" alt="Donate">
+                <h3>
+                    Donate Money to<br>Homeless Partners
+                </h3>
+                <div class="btnbox"><a href="http://www.homelesspartners.com/" class="btn btn-sm btn-block btn-primary">Donate</a></div>
+            </div>
+        </div>
+
+        <div class="col-md-4 portfolio-item">
+            <div class="transparentbox">
+                <img class="img-responsive" src="pledge_day_2014/images/SVG/chat.svg" alt="Listen">
+                <h3>
+                    Talk With a Homeless Person &amp; Hear Their Story
+                </h3>
+
+                <div class="btnbox"><a href="http://www.homelesspartners.com/" class="btn btn-sm btn-block btn-primary">View Stories</a></div>
+            </div>
+        </div>
+        <div class="col-md-2"> </div>
+    </div>
+
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-md-2"> </div>
+        <div class="col-md-8">
+            <div id="transparentboxlong2">
+                <h3>
+                    #PledgeDay2014
+                </h3>
+                <div id="spreadaware"><p>Spread awareness by sharing how you're making a difference <br>in your community by using this hashtag</p></div>
+                <a href="https://twitter.com/intent/tweet?button_hashtag=PledgeDay2014&text=I'm%20giving%20back%20to%20the%20community%20by...%20" class="twitter-hashtag-button" data-size="large" data-related="HomelessPartners">Tweet #PledgeDay2014</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"> </div>
+</div>
+
+<!-- row -->
+<div class="row">
+    <div class="col-md-12">
+        <h2>Sponsored By</h2>
+    </div>
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-md-2"> </div>
+    <div class="col-md-4">
+        <div class="logos">
+            <a href="http://www.vancitybuzz.com" target="_blank"><img class="img-responsive" src="pledge_day_2014/images/PNG/vancitybuzz.png" alt="Vancity Buzz"></a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="logos">
+            <a href="http://www.viralfoundry.com" target="_blank"><img class="img-responsive" src="pledge_day_2014/images/PNG/viralfoundry.png" alt="Viral Foundry"></a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-2"> </div>
+    <div class="col-md-4">
+        <div class="logos">
+            <a href="http://www.metropolisatmetrotown.com/" target="_blank"><img class="img-responsive" src="pledge_day_2014/images/PNG/metrotown.png" alt="Metrotown"></a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="logos">
+            <a href="http://www.hopewwc.org/" target="_blank"><img class="img-responsive" src="pledge_day_2014/images/PNG/hope.png" alt="Hope"></a>
+        </div>
+    </div>
+    <div class="col-md-2"> </div>
+</div>
+
+<!-- Pagination -->
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="spaceabove">Powered By <a href="http://www.homelesspartners.com/">Homeless Partners</a></p>
+        </div>
+    </div>
+    <!-- /.row -->
+</footer>
+<div class="row">
+    <hr>
+    <div class="col-md-12">
+        <h2>Share #PledgeDay2014 and It Will Appear Here</h2>
+    </div>
+</div>
+<!-- /.row -->
+<script type="text/javascript" class="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=tooaverage&height=1500&flexible=1&dont_load_new_posts=0&more_button=0&post_limit=5&embed_type=sidebar&show_rebelnav=1"></script>
+
+<hr>
+
+
+
+<!-- /.container -->
+<script>
+    $(document).ready(function(){
+        $("body").hide().fadeIn("slow");
+    });
+</script>
+
+<!-- jQuery -->
+<!-- Bootstrap Core JavaScript -->
+<script src="pledge_day_2014/js/bootstrap.min.js"></script>
+<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
+<script src="pledge_day_2014/js/vendor/jquery.min.js"></script>
+<script src="pledge_day_2014/js/vendor/video.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="pledge_day_2014/js/flat-ui-pro.js"></script>
+
+</body>
+
+</html>
